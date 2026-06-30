1093d187971d04def3610824cec4617e4627a9ab
1725110433-tech <1725110433@utectulancingo.edu.mx>
2026-06-30 16:58:20 +0000

    CREATED agenda

--- controllers/ver_contacto.py
+++ controllers/ver_contacto.py
@@ -5,6 +5,6 @@ render = web.template.render('views', nase='layout')
 
 class VerContacto:
 
-    def GET (self, id_contacto:int):
+    def GET (self,id_contacto):
         print(f"ID_CONTACTO: {id_contacto}")
         return render.ver_contacto()
